9e337bf277104a36f42ae96c394e0ee91642351d
Shinya Fujino <shf0811@gmail.com>
2020-08-27 16:38:14 +0900

    Use variables to describe colors

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '../styles/Navbar.module.scss'
+import styles from '../styles/Header.module.scss'
 
 const Header: React.FunctionComponent = () => {
   return (
